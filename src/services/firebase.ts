260e565b4b7ca6034008b468ca9d2f53c82f98d2
Jeong-eun, Kim <jee824k@gmail.com>
2023-10-21 01:13:45 +0900

    Init: Firebase 추가
    - 참고: https://firebase.google.com/docs/web/setup?authuser=0&hl=ko

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC3G3KFeEf4I6PZdk-8Ry6ZFf2fntjF0F4',
+  authDomain: 'cocoment-dev.firebaseapp.com',
+  projectId: 'cocoment-dev',
+  storageBucket: 'cocoment-dev.appspot.com',
+  messagingSenderId: '652317155962',
+  appId: '1:652317155962:web:a819a1390a1d2de0cbd1c3',
+};
+// const app =
+initializeApp(firebaseConfig);
